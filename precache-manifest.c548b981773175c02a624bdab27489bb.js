c659568efb44cc136667ec27414e45491ec963fc
sahil-github29 <sahil242927@gmail.com>
2020-04-09 11:18:31 +0530

    Updates

---
+++ precache-manifest.c548b981773175c02a624bdab27489bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b184a5c8aeb4a74078eb13930f81829f",
+    "url": "/react-robot-app/index.html"
+  },
+  {
+    "revision": "a1919f30141ee2a12e6f",
+    "url": "/react-robot-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "993187bf27c0670128d3",
+    "url": "/react-robot-app/static/js/2.d5cdfc34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robot-app/static/js/2.d5cdfc34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1919f30141ee2a12e6f",
+    "url": "/react-robot-app/static/js/main.157d17ab.chunk.js"
+  },
+  {
+    "revision": "7e40255556bf02537bf4",
+    "url": "/react-robot-app/static/js/runtime-main.b25c4d53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-robot-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robotreactapp/precache-manifest.afaad56fef8bf94f0980af30189def29.js"
+  "/react-robot-app/precache-manifest.c548b981773175c02a624bdab27489bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robotreactapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-robot-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
